666c7c05080f3b3d9e9577060594945773f96850
张天启 <lygztq@sjtu.edu.cn>
2017-12-17 13:10:05 +0800

    fix small mistake in hyper

--- hyper.py
+++ hyper.py
@@ -1,6 +1,6 @@
 hpyer = {
 	'DATA_PATH' : "./data/train/", 	# The training data path
-	'TEST_DATA_PATH':"./data/test/" # The test data path
+	'TEST_DATA_PATH':"./data/test/",# The test data path
 	'IMG_SIZE' : (41, 41), 			# The train data instance size
 	'BATCH_SIZE' : 64, 				# The Training batch size
 	'BASE_LR' : 0.0001,				# The initial learning step	
